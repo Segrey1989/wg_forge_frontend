9b91bb89181df292fd50261f85fd763f7957e570
Siarhei Kvachonak <segrey1989@tut.by>
2019-02-27 23:38:45 +0000

    feat: add styles by using js

---
+++ src/tasks/styleCreator.js
@@ -0,0 +1,18 @@
+const styles = {};
+styles['user-details'] = `
+  display: none;
+  position:absolute;
+  width: 40%;
+  background-color: black;
+  color: white;
+  opacity: 0.9;
+  text-align: center;
+  padding: 1%;
+  border-radius: 5px;
+  `;
+
+styles['link'] = `
+  
+  `;
+
+export default styles;
