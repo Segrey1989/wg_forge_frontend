3bf99458a740e54fa7f26c4d392e07d652c770b3
Siarhei Kvachonak <segrey1989@tut.by>
2019-03-06 12:37:41 +0000

    style: delete commented code

--- src/app.js
+++ src/app.js
@@ -2,13 +2,10 @@ import createTable from './tasks/Table';
 import dataStorage from './tasks/dataStorage';
 import data from './tasks/processData';
 
-// import { changeCurrencyEvent } from './tasks/events';
-
 export default (function() {
   const app = document.getElementById('app');
   data
     .then(result => {
-      // console.log(result.length);
       const table = createTable(dataStorage.ordersData);
       app.appendChild(table);
     })
